a402d4da1a3f121518dfd9d3a05238e548d1be87
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-02-09 10:37:19 +0000

    search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import Container from "./components/Container";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 export default function App() {
   const notes = useSelector((state) => state.notes.list);
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <Container>
       <Header />
+      <Search />
       <ul>
         {notes.map((note) => (
           <li key={note.id} style={{ backgroundColor: note.color }}>
---
+++ src/components/Search/index.js
@@ -0,0 +1,5 @@
+import styles from "./search.module.css";
+
+export default function Search() {
+  return <input className={styles.search} placeholder="Search..." />;
+}
